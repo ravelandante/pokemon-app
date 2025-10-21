b2568c6a81c02b9142ce6a9a399a230a98b2511c
Fynn <fynn@fynnyoung.com>
2025-10-21 12:26:06 +0200

    add basic pokemon list

--- src/components/Pokedex.tsx
+++ src/components/Pokedex.tsx
@@ -1,11 +1,14 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View, FlatList } from "react-native";
+import pokemonList from "../../assets/kanto.json";
 
 export default function Pokedex() {
 	return (
 		<View style={styles.container}>
-			<Text>Open up App.tsx to start working on your app!</Text>
-			<StatusBar style="auto" />
+			<FlatList
+				data={pokemonList}
+				renderItem={({ item }) => <div>{item.name}</div>}
+				keyExtractor={(item) => item.id.toString()}
+			/>
 		</View>
 	);
 }
