59743380731d6f99e42ff37e4a0e3c64decb754e
Fynn <fynn@fynnyoung.com>
2025-10-21 15:54:43 +0200

    navigate to PokemonDetails when clicking list item

--- src/components/PokemonListItem.tsx
+++ src/components/PokemonListItem.tsx
@@ -1,14 +1,19 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { useNavigation, ParamListBase } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
 interface PokemonListItemProps {
 	name: string;
 }
 
 export default function PokemonListItem({ name }: PokemonListItemProps) {
+	const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
 	return (
-		<View style={styles.listItem}>
-			<Text style={styles.text}>Name: {name}</Text>
-		</View>
+		<TouchableOpacity onPress={() => navigation.navigate("PokemonDetails")}>
+			<View style={styles.listItem}>
+				<Text style={styles.text}>Name: {name}</Text>
+			</View>
+		</TouchableOpacity>
 	);
 }
 
